5f72707b82b50bcd81edde89609f40068a92be90
avneeshverma1234 <avneesh_verma@yahoo.com>
2023-02-18 15:48:41 +0000

    Adding Subtract Method

--- tests/CalculatorTests.py
+++ tests/CalculatorTests.py
@@ -7,9 +7,11 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.calculator = Calculator();
 
-    def test_something(self):
+    def test_add_is_successful(self):
         self.assertEqual(self.calculator.add(2,2), 4,"Return value should be 4")
 
+    def test_subtract_is_successful(self):
+        self.assertEqual(self.calculator.subtract(5,4),1, "Result should be 1")
 
 if __name__ == '__main__':
     try:
